cec228c81231889f4b023ca8f80d0f6a69f078d5
Atif Hasan <robiulhasan.atif@gmail.com>
2022-01-25 11:19:48 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Atif's Portfolio website [React Project]
+
+## Live Preview: [Open Link][preview]
+
+Coded by - [Atif Hasan](http://instagram.com/atif_hasan_fahim/) | [FR Softwares](http://fr-softwares.netlify.app/)
+
+## Project Details
+
+A portfolio for a web designer. We used React js to make this portfolio. A clean design with full responsiveness. You will find this portfolio very professional. Also, we added smooth scroll in the portfolio which will make the scroll experience really elegant.
+
+This project is made for education purposes by the Team [FR Softwares][frsoftwares-website]. We are going to learn so many things through this project especially how to work with React Js. React Js is one of the hottest techs for web dev. Through this project, we will have a good understanding of react js. Besides React js we are going to use many other techs.
+
+## Project Requirement
+
+1. HTML, CSS
+1. JavaScript
+1. React Basic (optional)
+
+## What we are going to Use/learn
+
+- React
+- React Hooks
+- Styled Components
+- Swiper js
+- React Transition Group
+- Smooth Scrollbar
+- React Icons
+- React Router Dom
+- More...
+
+## want to use the website ?
+
+This website is made for educational purposes. Also, this will be free to use. Though a proper credit will be appreciated.
+
+# If you like the tutorial, please share this with others.
+
+[preview]: https://atif-hasan.netlify.app/
+[frsoftwares-website]: https://fr-softwares.netlify.app/
---
+++ src/components/Map.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import MapImg from '../assets/images/map.png';
+import PText from './PText';
+
+const MapStyles = styled.div`
+  background: url(${MapImg}) no-repeat;
+  background-position: center;
+  background-size: cover;
+  min-height: 400px;
+  .container {
+    position: relative;
+    min-height: 400px;
+  }
+  .map__card {
+    position: absolute;
+    right: 10%;
+    bottom: 10%;
+    padding: 2rem;
+    background: var(--deep-dark);
+    width: 100%;
+    max-width: 300px;
+    border-radius: 12px;
+  }
+  .map__card__heading {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  .map__card__link {
+    display: inline-block;
+    font-size: 1.6rem;
+    margin-top: 3rem;
+    text-decoration: underline;
+  }
+  @media only screen and (max-width: 768px) {
+    background-position: 80% center;
+  }
+  @media only screen and (max-width: 400px) {
+    .map__card {
+      max-width: none;
+      right: auto;
+    }
+  }
+`;
+
+export default function Map() {
+  return (
+    <MapStyles>
+      <div className="container">
+        <div className="map__card">
+          <h3 className="map__card__heading">Here is me</h3>
+          <PText>Jaleshwaritola, Bogura, Bangladesh</PText>
+          <a
+            className="map__card__link"
+            href="https://www.google.com/maps/place/Jalesharitola,+Bogura/@24.843849,89.3736399,17z/data=!4m5!3m4!1s0x39fc54e61b746c0f:0x8ba285389c73953a!8m2!3d24.8443229!4d89.3754382"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open in google map
+          </a>
+        </div>
+      </div>
+      {/* <img src={MapImg} alt="Map" /> */}
+    </MapStyles>
+  );
+}
